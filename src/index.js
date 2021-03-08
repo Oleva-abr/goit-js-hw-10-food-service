093e51ea28f4317a62022f0c88dba76720a82841
Olena Abramova <olena.v.abramova@gmail.com>
2021-03-08 18:38:52 +0200

    added menu gallery

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-// import './styles.css';
-import './main.scss';
+import './styles.css';
+import cardTemplates from './templates/templates.hbs';
+import menuCards from './menu.json';
+import './theme.js';
+
+const menuTemplatesCards = document.querySelector('.js-menu');
+const menuMarkup = cardTemplates(menuCards);
+
+menuTemplatesCards.insertAdjacentHTML('beforeend', menuMarkup);
