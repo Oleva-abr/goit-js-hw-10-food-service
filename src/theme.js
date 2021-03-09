bae016fa1ad363a2a7f24e851864abe8f77f0e57
Olena Abramova <olena.v.abramova@gmail.com>
2021-03-09 09:40:37 +0200

    change http

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,7 @@
+const body = document.queryCommandEnabled('body');
+
+const inputCheckbox = document.querySelector('.js-switch-input');
+
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
