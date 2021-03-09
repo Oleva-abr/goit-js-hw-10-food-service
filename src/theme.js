70b51f3065a5bab8a03e0e689423603df51cbdc9
Olena Abramova <olena.v.abramova@gmail.com>
2021-03-09 18:01:06 +0200

    Addet change theme

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,32 @@
-const body = document.queryCommandEnabled('body');
+const body = document.body;
 
-const inputCheckbox = document.querySelector('.js-switch-input');
+const inputCheckbox = document.querySelector('.theme-switch__toggle');
 
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+
+inputCheckbox.addEventListener('change', changeClass);
+
+function changeClass(event) {
+  if (event.currentTarget.checked) {
+    body.classList.add(Theme.DARK);
+    body.classList.remove(Theme.LIGHT);
+
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    body.classList.add(Theme.LIGHT);
+    body.classList.remove(Theme.DARK);
+
+    localStorage.removeItem('theme');
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
+
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme === Theme.DARK) {
+  body.classList.add(Theme.DARK);
+  inputCheckbox.checked = true;
+}
